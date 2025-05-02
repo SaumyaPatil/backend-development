723ae6e7ab1d238b6826587b148c8ba86c31b83a
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-02 12:37:28 +0530

    Architecture of Node.js

--- hello-world/file.js
+++ hello-world/file.js
@@ -24,22 +24,39 @@ const fs = require('fs');
 
 //Appending file - without over-writing content of file, how we can append then and there
 //The second arguement should always be a string so we need to convert it to string
-fs.appendFileSync("./test.txt", "Hey There\n");
+// fs.appendFileSync("./test.txt", "Hey There\n");
 
-//Copy file
-fs.cpSync("./test.txt", "./copy.txt");
+// //Copy file
+// fs.cpSync("./test.txt", "./copy.txt");
 
-//Delete file
-fs.unlinkSync("./copy.txt");
+// //Delete file
+// fs.unlinkSync("./copy.txt");
 
-//Stats of any file - when was it created, id, etc.
-console.log(fs.statSync("./test.txt"));
+// //Stats of any file - when was it created, id, etc.
+// console.log(fs.statSync("./test.txt"));
 
-console.log(fs.statSync("./test.txt").isFile());    //true
+// console.log(fs.statSync("./test.txt").isFile());    //true
 
-//creating folders
-fs.mkdirSync("my-docs");
+// //creating folders
+// fs.mkdirSync("my-docs");
 
-//creates function recursively
-fs.mkdirSync("my-docs/a/b", {recursive: true});
+// //creates function recursively
+// fs.mkdirSync("my-docs/a/b", {recursive: true});
+
+ 
+//This is blocking request
+console.log("1");
+const result = fs.readFileSync("contacts.txt", "utf-8");
+console.log(result);
+
+console.log("2");
+
+//This is non blocking request
+console.log("1");
+
+fs.readFile("contacts.txt", "utf-8", (err, res) => {
+    console.log(result);
+});
+
+console.log("2");
 
